1855cdcec63f038024b33f518da63a658f77e57c
ethannathaniel <mataeplatano@gmail.com>
2020-07-11 14:54:03 -0400

    primero

--- view/view_actor.php
+++ view/view_actor.php
@@ -7,6 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-    
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur reiciendis atque dolorem, mollitia aspernatur maiores iure! Veritatis, ex qui error dolore alias corporis voluptatem et, illo iusto, maiores ipsam modi?</p>    
 </body>
 </html>
